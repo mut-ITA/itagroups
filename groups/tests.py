8e074be3be55a0ff19695fd069f84bdde5ef23e7
ghust1995 <gustavo.ceci95@gmail.com>
2015-06-01 11:25:57 -0300

    First unit tests added

--- groups/tests.py
+++ groups/tests.py
@@ -1,6 +1,9 @@
 from django.core.urlresolvers import resolve
 from django.test import TestCase
 from django.http import HttpRequest
+from django.template.loader import render_to_string
+
+from groups.views import home_page
 
 # Create your tests here.
 
@@ -9,3 +12,9 @@ class HomePageTest(TestCase):
 	def test_root_url_resolves_to_home_page_view(self):
 		found = resolve('/')
 		self.assertEqual(found.func, home_page)
+
+	def test_home_page_returns_correct_html(self):
+		request = HttpRequest()
+		response = home_page(request)
+		expected_html = render_to_string('home.html')
+		self.assertEqual(response.content.decode(), expected_html)
--- itagroups/urls.py
+++ itagroups/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from groups import views
+
 urlpatterns = [
     # Examples:
     # url(r'^$', 'itagroups.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
+    #url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', views.home_page, name='home')
 ]
