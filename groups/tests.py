10e78fd206505f4037d6a9daa00fa858441f9ae9
ghust1995 <gustavo.ceci95@gmail.com>
2015-06-05 04:30:48 -0300

    Created test helper method file

--- groups/tests.py
+++ groups/tests.py
@@ -5,6 +5,7 @@ from django.template.loader import render_to_string
 
 from groups.views import home_page, search_groups
 from groups.models import Group
+from groups.HelperMethods.tests import create_sample_database, create_sample_POST_request
 
 # Create your tests here.
 
@@ -166,29 +167,3 @@ class SearchTests(TestCase):
 		found_groups = search_groups('description')
 		self.assertEqual(len(found_groups), 1)
 		self.assertTrue('tehalias' in [a.alias for a in found_groups])
-
-
-def create_sample_database():
-	first_group = Group()
-	first_group.name = 'Teh name'
-	first_group.alias = 'tehalias'
-	first_group.tags = 'Teh; tags'
-	first_group.description = 'Teh description' 
-	first_group.save()
-
-	second_group = Group()
-	second_group.name = 'Teh name2'
-	second_group.alias = 'tehalias2'
-	second_group.tags = 'Teh; tags2'
-	second_group.description = 'Teh description2'
-	second_group.save()	
-
-def create_sample_POST_request():
-	request = HttpRequest()
-	request.method = 'POST'
-	request.POST['group_name'] = 'New group name'
-	request.POST['group_alias'] = 'Newgroupalias'
-	request.POST['group_tags'] = 'New group tags'
-	request.POST['group_description'] = 'New group description'
-
-	return request
