1c2bb6987f955bbafb64a18f0f23e288a90102d8
ghust1995 <gustavo.ceci95@gmail.com>
2015-06-22 08:14:14 -0300

    Trying to fix stuff

--- itagroups/settings.py
+++ itagroups/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '%2pm9ehcf6+a1dcf9(cn^p9b+*3u*-h@zjhi!i)$eubvmh1yl3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["itagroups-staging.gustavomut.com"]
+ALLOWED_HOSTS = ["itagroups-staging.gustavomut.com", "localhost"]
 
 
 # Application definition
--- itagroups/urls.py
+++ itagroups/urls.py
@@ -6,8 +6,5 @@ from groups import views
 urlpatterns = [
     # Examples:
     url(r'^$', views.home_page, name='home'),
-    # url(r'^blog/', include('blog.urls')),
     url(r'^groups/(.+)/$', views.view_group, name='view_group')
-    #url(r'^admin/', include(admin.site.urls)),
-
 ]
